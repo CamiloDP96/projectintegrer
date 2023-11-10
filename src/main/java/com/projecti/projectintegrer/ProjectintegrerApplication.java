b199bb633e9e1fa0d529867eabce984170416437
CamiloDP96 <cadp.96@outlook.com>
2023-11-10 11:55:00 -0500

    first try

---
+++ src/main/java/com/projecti/projectintegrer/ProjectintegrerApplication.java
@@ -0,0 +1,13 @@
+package com.projecti.projectintegrer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectintegrerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectintegrerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projecti/projectintegrer/ProjectintegrerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projecti.projectintegrer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectintegrerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
